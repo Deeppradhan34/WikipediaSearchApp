828719dcbe787c90c9181e70bfa9422a72b67630
Deep Pradhan <deeppradhan34@gmail.com>
2018-12-26 21:09:25 +0530

    Update SearchResultsTableViewCell.swift

--- SearchWikipedia/SearchResultsTableViewCell.swift
+++ SearchWikipedia/SearchResultsTableViewCell.swift
@@ -32,141 +32,18 @@ class SearchResultsTableViewCell: UITableViewCell {
     }
     
     func downloadImage(imageUrl: String?) {
-        //        if let imgUrl = imageUrl, imgUrl.count > 0 {
-        //            DispatchQueue.global(qos: .default).async {//concuurent
-        //                let url = URL.init(string: imgUrl)
-        //                do {
-        //                    let data = try Data.init(contentsOf: url!)
-        //                    DispatchQueue.main.async {
-        //                        self.searchedImage?.image = UIImage.init(data: data)
-        //                    }
-        //                } catch {
-        //                    print("Failed")
-        //                }
-        //            }
-        //        }
-        //        if let imgUrl = imageUrl, imgUrl.count > 0 {
-        //            let queue = DispatchQueue(label: "SerialQueue")//serial
-        //            queue.async {
-        //
-        //                let url = URL.init(string: imgUrl)
-        //                do {
-        //                    let data = try Data.init(contentsOf: url!)
-        //                    DispatchQueue.main.async {
-        //                        self.searchedImage?.image = UIImage.init(data: data)
-        //                    }
-        //                } catch {
-        //                    print("Failed")
-        //                }
-        //            }
-        //        }
-        //        if let imgUrl = imageUrl, imgUrl.count > 0 {
-        //            for  i  in 1...5 {
-        //                DispatchQueue.global(qos: .background).sync {
-        //                    let url = URL.init(string: imgUrl)
-        //                    do {
-        //                        let data = try Data.init(contentsOf: url!)
-        //                        DispatchQueue.main.async {
-        //                            self.searchedImage?.image = UIImage.init(data: data)
-        //                        }
-        //                    } catch {
-        //                        print("Failed")
-        //                    }
-        //                }
-        //            }
-        //        }
-        
-        //        if let imgUrl = imageUrl, imgUrl.count > 0 {
-        //             for  i  in 1...5 {
-        //                let concurrentQueue = DispatchQueue.init(label: "sdds", attributes: .concurrent)
-        //            //    let sds = DispatchQueue.init(label: "sdsd", qos: .default, attributes: .concurrent, autoreleaseFrequency: .never, target: DispatchQueue.)
-        //
-        //            concurrentQueue.async {
-        //
-        //                let url = URL.init(string: imgUrl)
-        //                do {
-        //                    let data = try Data.init(contentsOf: url!)
-        //                    DispatchQueue.main.async {
-        //                        self.searchedImage?.image = UIImage.init(data: data)
-        //                    }
-        //                } catch {
-        //                    print("Failed")
-        //                }
-        //            }
-        //        }
-        // }
-        //        let operationQueue = OperationQueue()
-        //        let block = BlockOperation { () -> Void  in
-        //            let url = URL.init(string: imageUrl!)
-        //            do {
-        //                let data = try Data.init(contentsOf: url!)
-        //              //  DispatchQueue.main.async {
-        //                OperationQueue.main.addOperation {
-        //                    self.searchedImage?.image = UIImage.init(data: data)
-        //                }
-        //               // }
-        //            } catch {
-        //                print("Failed")
-        //            }
-        //            print("Hi")
-        //        }
-        //        block.completionBlock = {
-        //            print("block1 executed")
-        //        }
-        //        operationQueue.addOperation(block)
-        //        let block1 = BlockOperation { () -> Void in
-        //            print("Hey")
-        //        }
-        //
-        //        operationQueue.addOperation(block1)
-        
-         let operationQueue = OperationQueue()
-        let block = BlockOperation(block: {
-            for i in 1...3 {
-                if let imgUrl = imageUrl, imgUrl.count > 0 {
-                    let url = URL.init(string: imageUrl!)
-                    do {
-                        let data = try Data.init(contentsOf: url!)
-                        //  DispatchQueue.main.async {
-                        OperationQueue.main.addOperation {
-                            self.searchedImage?.image = UIImage.init(data: data)
-                        }
-                        // }
-                    } catch {
-                        print("Failed")
-                    }
-                    print("Hi")
-                }
-            }
-        })
-        block.completionBlock = {
-            print("block1 completed")
-        }
-        let block1 = BlockOperation(block: {
-            for i in 5...10 {
-                if let imgUrl = imageUrl, imgUrl.count > 0 {
-                    let url = URL.init(string: imageUrl!)
-                    do {
-                        let data = try Data.init(contentsOf: url!)
-                        //  DispatchQueue.main.async {
-                        OperationQueue.main.addOperation {
-                            self.searchedImage?.image = UIImage.init(data: data)
-                        }
-                        // }
-                    } catch {
-                        print("Failed")
-                    }
-                    print("Hi")
-                }
-            }
-        })
-        block1.completionBlock = {
-            print("block2 completed")
-        }
-        block1.addDependency(block)
-        operationQueue.addOperation(block)
-        operationQueue.addOperation(block1)
-        
-        
+               if let imgUrl = imageUrl, imgUrl.count > 0 {
+                   DispatchQueue.global(qos: .default).async {//concuurent
+                       let url = URL.init(string: imgUrl)
+                       do {
+                           let data = try Data.init(contentsOf: url!)
+                           DispatchQueue.main.async {
+                               self.searchedImage?.image = UIImage.init(data: data)
+                           }
+                       } catch {
+                           print("Failed")
+                       }
+                   }
+               }
     }
 }
